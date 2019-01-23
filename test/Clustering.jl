fafd4564ba3217619439e812f9d6ff1027819599
Thibaut Lienart <tlienart@me.com>
2019-01-23 17:48:32 +1100

    Simple Kmeans from Clusterin.jl with basic tests

---
+++ test/Clustering.jl
@@ -0,0 +1,38 @@
+module TestClustering
+
+using MLJ
+using Test
+using Random:seed!
+using LinearAlgebra:norm
+
+seed!(132442)
+
+task = load_crabs()
+
+X, y = X_and_y(task)
+X_array = convert(Matrix{Float64}, X)
+
+import Clustering
+
+barekm = KMeans()
+
+fitresult, cache, report = MLJ.fit(barekm, 1, X_array)
+
+r = MLJ.transform(barekm, fitresult, X_array)
+
+# distance from first point to second center
+@test r[1, 2] == norm(view(X_array, 1, :) .- view(fitresult.centers, :, 2))
+@test r[10, 3] == norm(view(X_array, 10, :) .- view(fitresult.centers, :, 3))
+
+p = MLJ.predict(barekm, fitresult, X_array)
+
+@test argmin(r[1, :]) == p[1]
+@test argmin(r[10, :]) == p[10]
+
+km = machine(barekm, X)
+
+# NOTE: this fails, no method matching fit(::KMeans, ::DataFrames.DataFrame)
+# fit!(km)
+
+end # module
+true
