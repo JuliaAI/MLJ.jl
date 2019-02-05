cbf207beab6178e00b916b83a8f872a1ca85127a
Yiannis Simillides <yiannis.simillides.14@ucl.ac.uk>
2019-02-05 16:07:45 +0000

    Update MLJ.jl

--- src/MLJ.jl
+++ src/MLJ.jl
@@ -133,7 +133,6 @@ function __init__()
     @load_interface DecisionTree "7806a523-6efd-50cb-b5f6-3fa6f1930dbb" lazy=true
     @load_interface Clustering "aaaa29a8-35af-508c-8bc3-b662a17a0fe5" lazy=true
     @load_interface GLM "38e38edf-8417-5370-95a0-9cbb8c7f171a" lazy=true
-    @load_interface ScikitLearn "3646fa90-6ef7-5e7e-9f22-8aca16db6324" lazy=true
 #    @load_interface  MultivariateStats "6f286f6a-111f-5878-ab1e-185364afe411" lazy=true
 end
 
