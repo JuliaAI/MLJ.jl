208f6bbad98904e51c76c37938fe897efeea849d
Julian P Samaroo <jpsamaroo@jpsamaroo.me>
2019-08-23 17:32:27 -0500

    Add parallelism tests for resampling

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,9 +3,14 @@
 # this test code be wrapped in a module. Any new module name will do -
 # eg, `module TestDatasets` for code testing `datasets.jl`.
 
+using Distributed
+addprocs(2)
+
+@everywhere begin
 using MLJ
 using MLJBase
 using Test
+end
 
 @constant junk=KNNRegressor()
 
@@ -100,5 +105,3 @@ const exdir = joinpath(MLJ.srcdir, "..", "examples")
 #     @test include(joinpath(exdir, "random_forest.jl"))
 #     @test include(joinpath(exdir, "two_parameter_tune.jl"))
 # end
-
-       
