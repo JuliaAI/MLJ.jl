80c9bc06bc1e265b416001b66848dcd11f5c39b2
Anthony D. Blaom <anthony.blaom@gmail.com>
2019-08-28 13:56:54 +1200

    reorganize networks.jl <---> composites.jl

--- test/composites.jl
+++ test/composites.jl
@@ -16,104 +16,107 @@ ytrain = yin[train]
 ridge_model = FooBarRegressor(lambda=0.1)
 selector_model = FeatureSelector()
 
-composite = MLJ.SimpleDeterministicCompositeModel(model=ridge_model,
+@testset "first test of hand-exported network" begin
+
+    composite = MLJ.SimpleDeterministicCompositeModel(model=ridge_model,
                                                   transformer=selector_model)
 
-fitresult, cache, report = MLJ.fit(composite, 3, Xtrain, ytrain)
-
-# to check internals:
-ridge = MLJ.machines(fitresult)[1]
-selector = MLJ.machines(fitresult)[2]
-ridge_old = deepcopy(ridge)
-selector_old = deepcopy(selector)
-
-# this should trigger no retraining:
-fitresult, cache, report =
-    @test_logs(
-        (:info, r"^Not"),
-        (:info, r"^Not"),
-        MLJ.update(composite, 2, fitresult, cache, Xtrain, ytrain))
-@test ridge.fitresult == ridge_old.fitresult
-@test selector.fitresult == selector_old.fitresult
-
-# this should trigger update of selector and training of ridge:
-selector_model.features = [:a, :b] 
-fitresult, cache, report =
-    @test_logs(
-        (:info, r"^Updating"),
-        (:info, r"^Training"),
-        MLJ.update(composite, 2, fitresult, cache, Xtrain, ytrain))
-@test ridge.fitresult != ridge_old.fitresult
-@test selector.fitresult != selector_old.fitresult
-ridge_old = deepcopy(ridge)
-selector_old = deepcopy(selector)
-
-# this should trigger updating of ridge only:
-ridge_model.lambda = 1.0
-fitresult, cache, report =
-    @test_logs(
+    fitresult, cache, report = MLJ.fit(composite, 3, Xtrain, ytrain)
+    
+    # to check internals:
+    ridge = MLJ.machines(fitresult)[1]
+    selector = MLJ.machines(fitresult)[2]
+    ridge_old = deepcopy(ridge)
+    selector_old = deepcopy(selector)
+    
+    # this should trigger no retraining:
+    fitresult, cache, report =
+        @test_logs(
+            (:info, r"^Not"),
+            (:info, r"^Not"),
+            MLJ.update(composite, 2, fitresult, cache, Xtrain, ytrain))
+    @test ridge.fitresult == ridge_old.fitresult
+    @test selector.fitresult == selector_old.fitresult
+    
+    # this should trigger update of selector and training of ridge:
+    selector_model.features = [:a, :b] 
+    fitresult, cache, report =
+        @test_logs(
+            (:info, r"^Updating"),
+            (:info, r"^Training"),
+            MLJ.update(composite, 2, fitresult, cache, Xtrain, ytrain))
+    @test ridge.fitresult != ridge_old.fitresult
+    @test selector.fitresult != selector_old.fitresult
+    ridge_old = deepcopy(ridge)
+    selector_old = deepcopy(selector)
+    
+    # this should trigger updating of ridge only:
+    ridge_model.lambda = 1.0
+    fitresult, cache, report =
+        @test_logs(
             (:info, r"^Not"),
             (:info, r"^Updating"),
             MLJ.update(composite, 2, fitresult, cache, Xtrain, ytrain))
-@test ridge.fitresult != ridge_old.fitresult
-@test selector.fitresult == selector_old.fitresult
+    @test ridge.fitresult != ridge_old.fitresult
+    @test selector.fitresult == selector_old.fitresult
+    
+    predict(composite, fitresult, MLJ.selectrows(Xin, test))
+    
+    Xs = source(Xtrain)
+    ys = source(ytrain)
+    
+    mach = machine(composite, Xs, ys)
+    yhat = predict(mach, Xs)
+    fit!(yhat, verbosity=3)
+    composite.transformer.features = [:b, :c]
+    fit!(yhat, verbosity=3)
+    fit!(yhat, rows=1:20, verbosity=3)
+    yhat(MLJ.selectrows(Xin, test))
 
-predict(composite, fitresult, MLJ.selectrows(Xin, test))
+end
 
-Xs = source(Xtrain)
-ys = source(ytrain)
-
-mach = machine(composite, Xs, ys)
-yhat = predict(mach, Xs)
-fit!(yhat, verbosity=3)
-composite.transformer.features = [:b, :c]
-fit!(yhat, verbosity=3)
-fit!(yhat, rows=1:20, verbosity=3)
-yhat(MLJ.selectrows(Xin, test))
-
-
-## EXPORTING A NETWORK BY HAND
+import MLJBase
+using ScientificTypes
 
 mutable struct WrappedRidge <: DeterministicNetwork
     ridge
 end
 
-function MLJ.fit(model::WrappedRidge, verbosity::Integer, X, y)
-    Xs = source(X)
-    ys = source(y)
+@testset "second test of hand-exported network" begin 
 
-    stand = Standardizer()
-    standM = machine(stand, Xs)
-    W = transform(standM, Xs)
-    
-    boxcox = UnivariateBoxCoxTransformer()
-    boxcoxM = machine(boxcox, ys)
-    z = transform(boxcoxM, ys)
+    function MLJ.fit(model::WrappedRidge, verbosity::Integer, X, y)
+        Xs = source(X)
+        ys = source(y)
         
-    ridgeM = machine(model.ridge, W, z)
-    zhat = predict(ridgeM, W)
-    yhat = inverse_transform(boxcoxM, zhat)
+        stand = Standardizer()
+        standM = machine(stand, Xs)
+        W = transform(standM, Xs)
+        
+        boxcox = UnivariateBoxCoxTransformer()
+        boxcoxM = machine(boxcox, ys)
+        z = transform(boxcoxM, ys)
+        
+        ridgeM = machine(model.ridge, W, z)
+        zhat = predict(ridgeM, W)
+        yhat = inverse_transform(boxcoxM, zhat)
+        
+        fit!(yhat)
+        return fitresults(Xs, ys, yhat)
+    end
+    
+    MLJBase.input_scitype(::Type{<:WrappedRidge}) = Table(Continuous)
+    MLJBase.target_scitype(::Type{<:WrappedRidge}) = AbstractVector{<:Continuous}
+    
+    ridge = FooBarRegressor(lambda=0.1)
+    model_ = WrappedRidge(ridge)
+    mach = machine(model_, Xin, yin)
+    fit!(mach)
+    yhat=predict(mach, Xin)
+    ridge.lambda = 1.0
+    fit!(mach)
+    @test predict(mach, Xin) != yhat
 
-    fit!(yhat)
-    return fitresults(Xs, ys, yhat)
 end
-
-import MLJBase
-using ScientificTypes
-MLJBase.input_scitype(::Type{<:WrappedRidge}) = Table(Continuous)
-MLJBase.target_scitype(::Type{<:WrappedRidge}) = AbstractVector{<:Continuous}
-
-ridge = FooBarRegressor(lambda=0.1)
-model = WrappedRidge(ridge)
-mach = machine(model, Xin, yin)
-fit!(mach)
-yhat=predict(mach, Xin)
-ridge.lambda = 1.0
-fit!(mach)
-@test predict(mach, Xin) != yhat
-
-
-## EXPORTING THE LEARNING NETWORK DEFINED BY A NODE
 
 x1 = map(n -> mod(n,3), rand(UInt8, 100)) |> categorical;
 x2 = randn(100);
@@ -122,106 +125,111 @@ y = x2.^2;
 
 @load DecisionTreeRegressor
 
-Xs = source(X)
-ys = source(y)
-z = log(ys)
-stand = UnivariateStandardizer()
-standM = machine(stand, z)
-u = transform(standM, z)
-hot = OneHotEncoder()
-hotM = machine(hot, Xs)
-W = transform(hotM, Xs)
-knn = KNNRegressor()
-knnM = machine(knn, W, u)
-oak = DecisionTreeRegressor()
-oakM = machine(oak, W, u)
-uhat = 0.5*(predict(knnM, W) + predict(oakM, W))
-zhat = inverse_transform(standM, uhat)
-yhat = exp(zhat)
+#@testset "test macro-exported supervised network" begin
 
-# test that state changes after fit:
-@test sum(MLJ.state(yhat) |> MLJ.flat_values) == 0
-fit!(yhat)
-@test sum(MLJ.state(W) |> MLJ.flat_values) == 1
+    Xs = source(X)
+    ys = source(y)
+    z = log(ys)
+    stand = UnivariateStandardizer()
+    standM = machine(stand, z)
+    u = transform(standM, z)
+    hot = OneHotEncoder()
+    hotM = machine(hot, Xs)
+    W = transform(hotM, Xs)
+    knn = KNNRegressor()
+    knnM = machine(knn, W, u)
+    oak = DecisionTreeRegressor()
+    oakM = machine(oak, W, u)
+    uhat = 0.5*(predict(knnM, W) + predict(oakM, W))
+    zhat = inverse_transform(standM, uhat)
+    yhat = exp(zhat)
+    
+    # test that state changes after fit:
+    @test sum(MLJ.state(yhat) |> MLJ.flat_values) == 0
+    fit!(yhat)
+    @test sum(MLJ.state(W) |> MLJ.flat_values) == 1
+    
+    # test nested reporting:
+    r = MLJ.report(yhat)
+    @test r isa NamedTuple
+    @test length(r.reports) == 4
+    @test r.reports[1] == NamedTuple()
+    
+    hot2 = deepcopy(hot)
+    knn2 = deepcopy(knn)
+    ys2 = source(nothing)
+    
+    # duplicate a network:
+    yhat2 = @test_logs((:warn, r"^No replacement"),
+                       replace(yhat, hot=>hot2, knn=>knn2, ys=>source(ys.data)))
+    
+    @test_logs((:info, r"^Train.*OneHot"),
+               (:info, r"^Spawn"),
+               (:info, r"^Train.*Univ"),
+               (:info, r"^Train.*KNN"),
+               (:info, r"^Train.*Dec"), fit!(yhat2))
+    @test length(MLJ.machines(yhat)) == length(MLJ.machines(yhat2))
+    @test models(yhat) == models(yhat2)
+    @test sources(yhat) == sources(yhat2)
+    @test MLJ.tree(yhat) == MLJ.tree(yhat2)
+    fit!(yhat2)
+    @test yhat() ≈ yhat2()
+    
+    # this change should trigger retraining of all machines except the
+    # univariate standardizer:
+    hot2.drop_last = true
+    @test_logs((:info, r"^Updating.*OneHot"),
+               (:info, r"^Spawn"),
+               (:info, r"^Not.*Univ"),
+               (:info, r"^Train.*KNN"),
+               (:info, r"^Train.*Dec"), fit!(yhat2))
+    
+    # export a supervised network:
+    model_ = @from_network(Composite(knn_rgs=knn, one_hot_enc=hot) <= (Xs, ys, yhat))
+    mach = machine(model_, X, y)
+    @test_logs((:info, r"^Train.*Composite"),
+               (:info, r"^Train.*OneHot"),
+               (:info, r"^Spawn"),
+               (:info, r"^Train.*Univ"),
+               (:info, r"^Train.*KNN"),
+               (:info, r"^Train.*Dec"), fit!(mach))
+    model_.knn_rgs.K = 5
+    @test_logs((:info, r"^Updat.*Composite"),
+               (:info, r"^Not.*OneHot"),
+               (:info, r"^Not.*Univ"),
+               (:info, r"^Updat.*KNN"),
+               (:info, r"^Not.*Dec"), fit!(mach))
+    
+    
+    # check data anomynity:
+    @test all(x->(x===nothing), [s.data for s in sources(mach.fitresult)])
 
-# test nested reporting:
-r = MLJ.report(yhat)
-@test r isa NamedTuple
-@test length(r.reports) == 4
-@test r.reports[1] == NamedTuple()
+#end
 
-hot2 = deepcopy(hot)
-knn2 = deepcopy(knn)
-ys2 = source(nothing)
+#@testset "test macro-exported unsupervised network" begin
 
-# duplicate a network:
-yhat2 = @test_logs((:warn, r"^No replacement"),
-           replace(yhat, hot=>hot2, knn=>knn2, ys=>source(ys.data)))
-
-@test_logs((:info, r"^Train.*OneHot"),
-           (:info, r"^Spawn"),
-           (:info, r"^Train.*Univ"),
-           (:info, r"^Train.*KNN"),
-           (:info, r"^Train.*Dec"), fit!(yhat2))
-@test length(MLJ.machines(yhat)) == length(MLJ.machines(yhat2))
-@test models(yhat) == models(yhat2)
-@test sources(yhat) == sources(yhat2)
-@test MLJ.tree(yhat) == MLJ.tree(yhat2)
-fit!(yhat2)
-@test yhat() ≈ yhat2()
-
-# this change should trigger retraining of all machines except the
-# univariate standardizer:
-hot2.drop_last = true
-@test_logs((:info, r"^Updating.*OneHot"),
-           (:info, r"^Spawn"),
-           (:info, r"^Not.*Univ"),
-           (:info, r"^Train.*KNN"),
-           (:info, r"^Train.*Dec"), fit!(yhat2))
-
-# export a supervised network:
-model_ = @from_network(Composite(knn_rgs=knn, one_hot_enc=hot) <=
-                      (Xs, ys, yhat))
-mach = machine(model_, X, y)
-@test_logs((:info, r"^Train.*Composite"),
-           (:info, r"^Train.*OneHot"),
-           (:info, r"^Spawn"),
-           (:info, r"^Train.*Univ"),
-           (:info, r"^Train.*KNN"),
-           (:info, r"^Train.*Dec"), fit!(mach))
-model_.knn_rgs.K = 5
-@test_logs((:info, r"^Updat.*Composite"),
-           (:info, r"^Not.*OneHot"),
-           (:info, r"^Not.*Univ"),
-           (:info, r"^Updat.*KNN"),
-           (:info, r"^Not.*Dec"), fit!(mach))
-
-
-# check data anomynity:
-@test all(x->(x===nothing), [s.data for s in sources(mach.fitresult)])
-
-# export an unsupervised model
-multistand = Standardizer()
-multistandM = machine(multistand, W)
-W2 = transform(multistandM, W)
-model_ = @from_network Transf(one_hot=hot) <= (Xs, W2)
-mach = machine(model_, X)
-@test_logs((:info, r"^Training.*Transf"),
-           (:info, r"^Train.*OneHot"),
-           (:info, r"^Spawn"),
-           (:info, r"Train.*Stand"), fit!(mach))
-model_.one_hot.drop_last=true
-@test_logs((:info, r"^Updating.*Transf"),
-           (:info, r"^Updating.*OneHot"),
-           (:info, r"^Spawn"),
-           (:info, r"Train.*Stand"), fit!(mach))
-@test(fitted_params(mach).fitted_params[1] isa
-      NamedTuple{(:mean_and_std_given_feature,)})
-
-# check data anomynity:
-@test all(x->(x===nothing), [s.data for s in sources(mach.fitresult)])
-
-transform(mach)
+    multistand = Standardizer()
+    multistandM = machine(multistand, W)
+    W2 = transform(multistandM, W)
+    model_ = @from_network Transf(one_hot=hot) <= (Xs, W2)
+    mach = machine(model_, X)
+    @test_logs((:info, r"^Training.*Transf"),
+               (:info, r"^Train.*OneHot"),
+               (:info, r"^Spawn"),
+               (:info, r"Train.*Stand"), fit!(mach))
+    model_.one_hot.drop_last=true
+    @test_logs((:info, r"^Updating.*Transf"),
+               (:info, r"^Updating.*OneHot"),
+               (:info, r"^Spawn"),
+               (:info, r"Train.*Stand"), fit!(mach))
+    @test(fitted_params(mach).fitted_params[1] isa
+          NamedTuple{(:mean_and_std_given_feature,)})
+    
+    # check data anomynity:
+    @test all(x->(x===nothing), [s.data for s in sources(mach.fitresult)])
+    
+    transform(mach)
+#end
 
 end
 true
--- test/networks.jl
+++ test/networks.jl
@@ -8,161 +8,166 @@ using CategoricalArrays
 import Random.seed!
 seed!(1234)
 
-N =100
-X = (x1=rand(N), x2=rand(N), x3=rand(N))
-y = 2X.x1  - X.x2 + 0.05*rand(N)
 
-knn_ = KNNRegressor(K=7)
+@testset "network #1" begin
 
-# split the rows:
-allrows = eachindex(y);
-train, valid, test = partition(allrows, 0.7, 0.15);
-@test vcat(train, valid, test) == allrows
+    N =100
+    X = (x1=rand(N), x2=rand(N), x3=rand(N))
+    y = 2X.x1  - X.x2 + 0.05*rand(N)
+    
+    knn_ = KNNRegressor(K=7)
+    
+    # split the rows:
+    allrows = eachindex(y);
+    train, valid, test = partition(allrows, 0.7, 0.15);
+    @test vcat(train, valid, test) == allrows
+    
+    Xtrain = selectrows(X, train)
+    ytrain = y[train]
+    
+    Xs = source(Xtrain)
+    ys = source(ytrain)
+    
+    knn1 = machine(knn_, Xs, ys)
+    @test_logs (:info, r"Training") fit!(knn1, verbosity=3)
+    knn_.K = 5
+    @test_logs (:info, r"Training") fit!(knn1, rows=train[1:end-10], verbosity=2)
+    @test_logs (:info, r"Training") fit!(knn1, verbosity=2)
+    yhat = predict(knn1, Xs)
+    yhat(selectrows(X, test))
+    @test rms(yhat(selectrows(X, test)), y[test]) < 0.3
+    @test MLJ.is_stale(knn1) == false
 
-Xtrain = selectrows(X, train)
-ytrain = y[train]
+end
 
-Xs = source(Xtrain)
-ys = source(ytrain)
+@testset "network #2" begin
+    
+    N =100
+    X = (x1=rand(N),
+         x2=rand(N),
+         x3=categorical(rand("yn",N)),
+         x4=categorical(rand("yn",N)))
+    
+    y = 2X.x1  - X.x2 + 0.05*rand(N)
+    X = source(X)
+    y = source(y)
+    
+    hot = OneHotEncoder()
+    hotM = machine(hot, X)
+    W = transform(hotM, X)
+    knn = KNNRegressor()
+    knnM = machine(knn, W, y)
+    yhat = predict(knnM, W)
+    
+    # should get "Training" for both:
+    @test_logs (:info, r"^Training") (:info, r"^S") (:info, r"^S") (:info, r"^Training") fit!(yhat)
+    
+    # should get "Not retraining" for both:
+    @test_logs (:info, r"^Not retraining") (:info, r"^Not retraining") fit!(yhat)
+    
+    # should get "Updating" for first, "Training" for second:
+    hot.drop_last = true
+    @test_logs (:info, r"^Updating")  (:info, r"^S") (:info, r"^S") (:info, r"^Training") fit!(yhat)
+    
+    # should get "Not retraining" for both:
+    @test_logs (:info, r"^Not retraining") (:info, r"^Not retraining") fit!(yhat)
+    
+    # should get "Not retraining" for first, "Updating for second":
+    knn.K = 17
+    @test_logs (:info, r"^Not retraining") (:info, r"^Updating") fit!(yhat)
+    
+    # should get "Training" for both:
+    @test_logs (:info, r"^Training")  (:info, r"^S") (:info, r"^S") (:info, r"^Training") fit!(yhat, rows=1:100)
+    
+    # should get "Training" for both"
+    @test_logs (:info, r"^Training")  (:info, r"^S") (:info, r"^S") (:info, r"^Training") fit!(yhat)
+    
+    # should get "Training" for both"
+    @test_logs (:info, r"^Training")  (:info, r"^S") (:info, r"^S") (:info, r"^Training") fit!(yhat, force=true)
+    
+    forest = EnsembleModel(atom=ConstantRegressor(), n=4)
+    forestM = machine(forest, W, y)
+    zhat = predict(forestM, W)
+    @test_logs (:info, r"^Not") (:info, r"^Train") fit!(zhat)
+    forest.n = 6
+    @test_logs (:info, r"^Not") (:info, r"^Updating") (:info, r"Build.*length 4") fit!(zhat)
 
-knn1 = machine(knn_, Xs, ys)
-@test_logs (:info, r"Training") fit!(knn1, verbosity=3)
-knn_.K = 5
-@test_logs (:info, r"Training") fit!(knn1, rows=train[1:end-10], verbosity=2)
-@test_logs (:info, r"Training") fit!(knn1, verbosity=2)
-yhat = predict(knn1, Xs)
-yhat(selectrows(X, test))
-@test rms(yhat(selectrows(X, test)), y[test]) < 0.3
-@test MLJ.is_stale(knn1) == false
+end
+    
+@testset "network #3" begin
+    
+    N =100
+    X = (x1=rand(N), x2=rand(N), x3=rand(N))
+    y = 2X.x1  - X.x2 + 0.05*rand(N)
+    
+    XX = source(X)
+    yy = source(y)
+    
+    # construct a transformer to standardize the target:
+    uscale_ = UnivariateStandardizer()
+    uscale = machine(uscale_, yy)
+    
+    # get the transformed inputs, as if `uscale` were already fit:
+    z = transform(uscale, yy)
+    
+    # construct a transformer to standardize the inputs:
+    scale_ = Standardizer() 
+    scale = machine(scale_, XX) # no need to fit
+    
+    # get the transformed inputs, as if `scale` were already fit:
+    Xt = transform(scale, XX)
+    
+    # do nothing to the DataFrame
+    Xa = node(identity, Xt)
+    
+    # choose a learner and make it machine:
+    knn_ = KNNRegressor(K=7) # just a container for hyperparameters
+    knn = machine(knn_, Xa, z) # no need to fit
+    
+    # get the predictions, as if `knn` already fit:
+    zhat = predict(knn, Xa)
+    
+    # inverse transform the target:
+    yhat = inverse_transform(uscale, zhat)
+    
+    # fit-through training:
+    @test_logs((:info, r"Training"),
+               (:info, r"Features standarized: "),
+               (:info, r" *:x1"),
+               (:info, r" *:x2"),
+               (:info, r" *:x3"),
+               (:info, r"Training"),
+               (:info, r"Training"),
+               fit!(yhat, rows=1:50, verbosity=2))
+    @test_logs(
+        (:info, r"Not retraining"),
+        (:info, r"Not retraining"),
+        (:info, r"Not retraining"),
+        fit!(yhat, rows=1:50, verbosity=1))
+    @test_logs(
+        (:info, r"Training"),
+        (:info, r"Training"),
+        (:info, r"Training"),
+        fit!(yhat, verbosity=1))
+    knn_.K =67
+    @test_logs(
+        (:info, r"Not retraining"),
+        (:info, r"Not retraining"),
+        (:info, r"Updating"),
+        fit!(yhat, verbosity=1))
 
+end
 
-# FIRST TEST OF NETWORK TRAINING
-
-N =100
-X = (x1=rand(N),
-     x2=rand(N),
-     x3=categorical(rand("yn",N)),
-     x4=categorical(rand("yn",N)))
-
-y = 2X.x1  - X.x2 + 0.05*rand(N)
-X = source(X)
-y = source(y)
-
-hot = OneHotEncoder()
-hotM = machine(hot, X)
-W = transform(hotM, X)
-knn = KNNRegressor()
-knnM = machine(knn, W, y)
-yhat = predict(knnM, W)
-
-# should get "Training" for both:
-@test_logs (:info, r"^Training") (:info, r"^S") (:info, r"^S") (:info, r"^Training") fit!(yhat)
-
-# should get "Not retraining" for both:
-@test_logs (:info, r"^Not retraining") (:info, r"^Not retraining") fit!(yhat)
-
-# should get "Updating" for first, "Training" for second:
-hot.drop_last = true
-@test_logs (:info, r"^Updating")  (:info, r"^S") (:info, r"^S") (:info, r"^Training") fit!(yhat)
-
-# should get "Not retraining" for both:
-@test_logs (:info, r"^Not retraining") (:info, r"^Not retraining") fit!(yhat)
-
-# should get "Not retraining" for first, "Updating for second":
-knn.K = 17
-@test_logs (:info, r"^Not retraining") (:info, r"^Updating") fit!(yhat)
-
-# should get "Training" for both:
-@test_logs (:info, r"^Training")  (:info, r"^S") (:info, r"^S") (:info, r"^Training") fit!(yhat, rows=1:100)
-
-# should get "Training" for both"
-@test_logs (:info, r"^Training")  (:info, r"^S") (:info, r"^S") (:info, r"^Training") fit!(yhat)
-
-# should get "Training" for both"
-@test_logs (:info, r"^Training")  (:info, r"^S") (:info, r"^S") (:info, r"^Training") fit!(yhat, force=true)
-
-
-## SECOND TEST OF NETWORK TRAINING
-
-# test smart updating of an ensemble within a network:
-forest = EnsembleModel(atom=ConstantRegressor(), n=4)
-forestM = machine(forest, W, y)
-zhat = predict(forestM, W)
-@test_logs (:info, r"^Not") (:info, r"^Train") fit!(zhat)
-forest.n = 6
-@test_logs (:info, r"^Not") (:info, r"^Updating") (:info, r"Build.*length 4") fit!(zhat)
-
-
-## THIRD TEST OF NETWORK TRAINING
-
-N =100
-X = (x1=rand(N), x2=rand(N), x3=rand(N))
-y = 2X.x1  - X.x2 + 0.05*rand(N)
-
-XX = source(selectrows(X, train))
-yy = source(y[train])
-
-# construct a transformer to standardize the target:
-uscale_ = UnivariateStandardizer()
-uscale = machine(uscale_, yy)
-
-# get the transformed inputs, as if `uscale` were already fit:
-z = transform(uscale, yy)
-
-# construct a transformer to standardize the inputs:
-scale_ = Standardizer() 
-scale = machine(scale_, XX) # no need to fit
-
-# get the transformed inputs, as if `scale` were already fit:
-Xt = transform(scale, XX)
-
-# do nothing to the DataFrame
-Xa = node(identity, Xt)
-
-# choose a learner and make it machine:
-knn_ = KNNRegressor(K=7) # just a container for hyperparameters
-knn = machine(knn_, Xa, z) # no need to fit
-
-# get the predictions, as if `knn` already fit:
-zhat = predict(knn, Xa)
-
-# inverse transform the target:
-yhat = inverse_transform(uscale, zhat)
-
-# fit-through training:
-@test_logs((:info, r"Training"),
-           (:info, r"Features standarized: "),
-           (:info, r" *:x1"),
-           (:info, r" *:x2"),
-           (:info, r" *:x3"),
-           (:info, r"Training"),
-           (:info, r"Training"),
-           fit!(yhat, rows=1:50, verbosity=2))
-@test_logs(
-           (:info, r"Not retraining"),
-           (:info, r"Not retraining"),
-           (:info, r"Not retraining"),
-           fit!(yhat, rows=1:50, verbosity=1))
-@test_logs(
-           (:info, r"Training"),
-           (:info, r"Training"),
-           (:info, r"Training"),
-           fit!(yhat, verbosity=1))
-knn_.K =67
-@test_logs(
-           (:info, r"Not retraining"),
-           (:info, r"Not retraining"),
-           (:info, r"Updating"),
-           fit!(yhat, verbosity=1))
-
-A  = rand(2,3)
-As = source(A)
-@test MLJ.matrix(MLJ.table(As))() == A
-
-y = rand(4)
-ys = source(y)
-@test vcat(ys, ys)() == vcat(y, y)
-@test hcat(ys, ys)() == hcat(y, y)
+@testset "overloading methods for AbstractNode" begin
+    A  = rand(2,3)
+    As = source(A)
+    @test MLJ.matrix(MLJ.table(As))() == A
+    
+    y = rand(4)
+    ys = source(y)
+    @test vcat(ys, ys)() == vcat(y, y)
+    @test hcat(ys, ys)() == hcat(y, y)
+end
 
 end
 
