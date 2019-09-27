5c1ab107c4fe80d77bba5f39d7cde09fe828485c
Anthony D. Blaom <anthony.blaom@gmail.com>
2019-09-28 05:55:46 +1200

    make exported learning networks mutable structs

--- src/composites.jl
+++ src/composites.jl
@@ -308,7 +308,7 @@ function from_network_(modl, modeltype_ex, fieldname_exs, model_exs,
     # code defining the composite model struct and fit method:
     program1 = quote
 
-        struct $modeltype_ex <: MLJ.$kind
+        mutable struct $modeltype_ex <: MLJ.$kind
             $(fieldname_exs...)
         end
 
