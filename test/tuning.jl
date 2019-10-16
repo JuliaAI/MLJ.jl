2a270f89d366f7f04c4fb220aeed4089492c15e3
Anthony D. Blaom <anthony.blaom@gmail.com>
2019-10-16 17:42:52 +1300

    add multi-resolution grid search #269

--- test/tuning.jl
+++ test/tuning.jl
@@ -18,24 +18,24 @@ X = (x1=x1, x2=x2, x3=x3);
 y = 2*x1 .+ 5*x2 .- 3*x3 .+ 0.2*rand(100);
 
 @testset "2-parameter tune, no nesting" begin
-    
+
     sel = FeatureSelector()
     stand = UnivariateStandardizer()
     ridge = FooBarRegressor()
     composite = MLJ.SimpleDeterministicCompositeModel(transformer=sel, model=ridge)
-    
+
     features_ = range(composite, :(transformer.features), values=[[:x1], [:x1, :x2], [:x2, :x3], [:x1, :x2, :x3]])
     lambda_ = range(composite, :(model.lambda), lower=1e-6, upper=1e-1, scale=:log10)
-    
+
     ranges = [features_, lambda_]
-    
+
     holdout = Holdout(fraction_train=0.8)
     grid = Grid(resolution=10)
-    
+
     tuned_model = TunedModel(model=composite, tuning=grid,
                              resampling=holdout, measure=rms,
                              ranges=ranges, full_report=false)
-    
+
     MLJBase.info_dict(tuned_model)
 
     tuned = machine(tuned_model, X, y)
@@ -47,18 +47,19 @@ y = 2*x1 .+ 5*x2 .- 3*x3 .+ 0.2*rand(100);
     report(tuned)
 
     b = fitted_params(tuned).best_model
-    
+
     measurements = tuned.report.measurements
     # should be all different:
     @test length(unique(measurements)) == length(measurements)
-    
+
     @test length(b.transformer.features) == 3
     @test abs(b.model.lambda - 0.027825) < 1e-6
-    
+
     # get the training error of the tuned_model:
     e = rms(y, predict(tuned, X))
 
-    # check this error has same order of magnitude as best measurement during tuning:
+    # check this error has same order of magnitude as best measurement
+    # during tuning:
     r = e/tuned.report.best_measurement
     @test r < 10 && r > 0.1
 
@@ -69,21 +70,57 @@ y = 2*x1 .+ 5*x2 .- 3*x3 .+ 0.2*rand(100);
 
 end
 
-@testset "one parameter tune" begin
-    ridge = FooBarRegressor()
-    r = range(ridge, :lambda, lower=1e-7, upper=1e-3, scale=:log)
-    tuned_model = TunedModel(model=ridge, ranges=r)
-    tuned = machine(tuned_model, X, y)
-    fit!(tuned)
-    report(tuned)
-end
+#@testset "multiple resolutions" begin
+
+    forest = EnsembleModel(atom=@load(KNNRegressor))
+    pipe = @pipeline MyPipe(sel=FeatureSelector(), forest=forest)
+
+    X = MLJ.table(rand(150, 2), names=[:x1, :x2])
+    y = rand(150)
+
+    r1 = range(pipe, :(sel.features), values=[[:x1,], [:x1, :x2]])
+    r2 = range(pipe, :(forest.atom.K), lower = 1, upper=50)
+    r3 = range(pipe, :(forest.bagging_fraction), lower=0.5, upper=1.0)
+    ranges = [r1, r2, r3]
+
+    holdout = Holdout(fraction_train=0.8)
+    grid = Grid(resolution=5)
+
+    tuned = TunedModel(model=pipe, tuning=grid,
+                       resampling=holdout, measure=rms,
+                       ranges=ranges)
+
+    mach = machine(tuned, X, y)
+
+    function num_grid_points(resolution)
+        grid.resolution = resolution
+        fit!(mach)
+        return report(mach).measurements |> length
+    end
+
+    @test num_grid_points(6) == 72
+    @test num_grid_points([:(forest.atom.K)=>7,
+                           :(forest.bagging_fraction)=>4]) == 56
+    @test num_grid_points([:(forest.atom.K)=>6, ]) == 60
+
+
+    @testset "one parameter tune" begin
+        ridge = FooBarRegressor()
+        r = range(ridge, :lambda, lower=1e-7, upper=1e-3, scale=:log)
+        tuned_model = TunedModel(model=ridge, ranges=r)
+        tuned = machine(tuned_model, X, y)
+        fit!(tuned)
+        report(tuned)
+    end
+
+#end
 
 @testset "nested parameter tune" begin
     tree_model = KNNRegressor()
     forest_model = EnsembleModel(atom=tree_model)
     r1 = range(forest_model, :(atom.K), lower=3, upper=4)
     r2 = range(forest_model, :bagging_fraction, lower=0.4, upper=1.0);
-    self_tuning_forest_model = TunedModel(model=forest_model, 
+    self_tuning_forest_model = TunedModel(model=forest_model,
                                           tuning=Grid(resolution=3),
                                           resampling=Holdout(),
                                           ranges=[r1, r2],
