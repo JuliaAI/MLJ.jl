f8973c705ff4eae1a05af1886381f9cd1b8b68a5
Anthony D. Blaom <anthony.blaom@gmail.com>
2019-11-05 14:35:37 +1300

    add machine support of weights; test some warnings for constructor

--- test/machines.jl
+++ test/machines.jl
@@ -6,7 +6,7 @@ using MLJBase
 using Test
 using Statistics
 
-@load KNNRegressor
+@load DecisionTreeRegressor
 
 N=50
 X = (a=rand(N), b=rand(N), c=rand(N))
@@ -14,12 +14,14 @@ y = 2*X.a - X.c + 0.05*rand(N)
 
 train, test = partition(eachindex(y), 0.7);
 
-t = Machine(KNNRegressor(K=4), X, y)
+tree = DecisionTreeRegressor(max_depth=5)
+
+t = Machine(tree, X, y)
 @test_logs (:info, r"Training") fit!(t)
 @test_logs (:info, r"Training") fit!(t, rows=train)
 @test_logs (:info, r"Not retraining") fit!(t, rows=train)
 @test_logs (:info, r"Training") fit!(t)
-MLJ.recursive_setproperty!(t, :(model.K),  5)
+MLJ.recursive_setproperty!(t, :(model.max_depth),  1)
 @test_logs (:info, r"Updating") fit!(t)
 
 predict(t, selectrows(X,test))
@@ -42,5 +44,14 @@ stand = machine(Standardizer(), source((x1=rand(10),)))
 freeze!(stand)
 @test_logs (:warn, r"not trained as it is frozen\.$") fit!(stand)
 
+@testset "warnings" begin
+    @test_logs((:warn, r"DecisionTreeRegressor does not support"),
+               machine(tree, X, y, rand(N)))
+    @test_throws DimensionMismatch machine(tree, X, y[1:end-1])
+    @test_throws DimensionMismatch machine(tree, X, y, rand(N-1))
+    @test_logs((:warn, r"The scitype of `y`"),
+               machine(tree, X, categorical(1:N)))
+    @test_logs((:warn, r"The scitype of `X`"),
+               machine(tree, (x=categorical(1:N),), y))
 end # module
 true
