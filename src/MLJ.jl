cbf316bde53a943d8e8db802a7214a522a99a761
Thibaut Lienart <tlienart@me.com>
2019-11-06 15:17:59 +0100

    Hard deps and run docs only on #master (#322)
    * remove hard deps + travis fix

--- src/MLJ.jl
+++ src/MLJ.jl
@@ -66,6 +66,11 @@ export models, localmodels, @load, load, info,
 using MLJBase
 using MLJModels
 
+# these are defined in MLJBase
+export load_boston, load_ames, load_iris
+export load_reduced_ames
+export load_crabs
+
 # to be extended:
 import MLJBase: fit, update, clean!,
     predict, predict_mean, predict_median, predict_mode,
@@ -73,22 +78,23 @@ import MLJBase: fit, update, clean!,
     show_as_constructed, params
 import MLJModels: models
 
-using Requires
-import Pkg.TOML
-import Pkg
-using OrderedCollections
+import Pkg, Pkg.TOML
+using Tables, OrderedCollections
+
 using  CategoricalArrays
-import Distributions: pdf, mode
 import Distributions
-import StatsBase
+import Distributions: pdf, mode
+import Statistics, StatsBase, LinearAlgebra, Random
+import Random: AbstractRNG, MersenneTwister
+
 using ProgressMeter
-import Tables
+
 import PrettyTables
-import Random
 using ScientificTypes
-import ScientificTypes
+
 using ComputationalResources
-import ComputationalResources: CPUProcesses
+using ComputationalResources: CPUProcesses
+
 const DEFAULT_RESOURCE = Ref{AbstractResource}(CPU1())
 
 # convenience packages
@@ -99,9 +105,6 @@ import Base: ==, getindex, setindex!
 import StatsBase.fit!
 
 # from Standard Library:
-using Statistics
-using LinearAlgebra
-using Random
 import Distributed: @distributed, nworkers, pmap
 using RecipesBase # for plotting
 
@@ -117,7 +120,6 @@ const CategoricalElement = Union{CategoricalString,CategoricalValue}
 # and https://github.com/JuliaLang/Pkg.jl/pull/1086/commits/996c6b9b69ef0c058e0105427983622b7cc8cb1d
 toml = Pkg.TOML.parsefile(joinpath(dirname(dirname(pathof(MLJ))), "Project.toml"))
 const MLJ_VERSION = toml["version"]
-
 
 ## INCLUDES
 
@@ -140,14 +142,5 @@ include("model_matching.jl")# inferring model search criterion from data
 include("tasks.jl")         # enhancements to MLJBase task interface
 include("scitypes.jl")      # extensions to ScientificTypes.sictype
 include("plotrecipes.jl")
-
-
-## INCLUDES FOR OPTIONAL DEPENDENCIES
-
-function __init__()
-    @require(CSV="336ed68f-0bac-5ca0-87d4-7b16caf5d00b",
-             include("datasets_requires.jl"))
-end
-
 
 end # module
