e6acc8b1b314d594263c0115feb6541e9e922d29
Anthony D. Blaom <anthony.blaom@gmail.com>
2020-10-12 18:20:35 +1300

    typo

--- docs/make.jl
+++ docs/make.jl
@@ -47,7 +47,7 @@ pages = [
     "FAQ" => "frequently_asked_questions.md",
     "Julia BlogPost" => "julia_blogpost.md",
     "Acceleration and Parallelism" => "acceleration_and_parallelism.md",
-    "Index of methods" => "api.md",
+    "Index of Methods" => "api.md",
     ]
 
 for (k, v) in pages
