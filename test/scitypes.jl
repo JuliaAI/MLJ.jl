b6582d34e400959996e77c23cc8ad8d599ed1052
Anthony D. Blaom <anthony.blaom@gmail.com>
2021-12-28 16:39:26 +1300

    fix scitpyes for models (experimental, not public API)

--- test/scitypes.jl
+++ test/scitypes.jl
@@ -1,17 +1,12 @@
 module TestScitypes
 
 using MLJ
-import MLJBase
 using Test
 
 S = scitype(ConstantClassifier())
 @test S().prediction_type == :probabilistic
 U = scitype(FeatureSelector())
 @test U().input_scitype == MLJ.Table
-
-# XXX fix this in the next release cycle (Nov 5, 2019)
-# M = scitype(rms)
-# @test_broken M().prediction_type == :deterministic
 
 @test scitype(OneHotEncoder()) ==
     MLJ.UnsupervisedScitype{Table,Table}
